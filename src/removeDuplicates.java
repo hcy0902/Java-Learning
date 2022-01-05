2ddebbe66b246858d66a7a79c26079ca98423dec
CORPAA\224863 <alexhector.lattanzi@aa.com>
2022-01-04 18:54:41 -0600

    Jan 4th | removeDuplicates. Key Point: HOW to iterate through a map, HOW to increment map's value, HOW to convert ArrayList to int[]

--- src/removeDuplicates.java
+++ src/removeDuplicates.java
@@ -1,10 +1,7 @@
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class removeDuplicates {
 
@@ -21,31 +18,32 @@ public class removeDuplicates {
 //
 //        getWords(testString);
 //
-        int[] nums = {1,2,2,2,3,3,4,5,7,7,9,10,11,13,13,16,19,20};
-        System.out.println(Arrays.toString(findDuplicate(nums)));
+
 //        System.out.println();
 //
-        String test = "   I  Love    Java \n";
-        String moreTest = "This is a test";
-        System.out.print(test.replaceAll("\\s", ""));
+//        String test = "   I  Love    Java \n";
+//        String moreTest = "This is a test";
+//        System.out.print(test.replaceAll("\\s", ""));
 //        System.out.println(moreTest);
 
 //        System.out.println(inputDate);
 //        System.out.println(formatIssueDateFromRemarks);
 
-        String dateNeedFormat = "10Nov22";
-        String expirationDate = null;
-        String actualDate = formatDate(dateNeedFormat);
-        String expectedDate = "2022-11-10";
-        System.out.println(formatDate("2022-11-10"));
-        System.out.println(actualDate);
-        System.out.println(expectedDate);
-        System.out.println(formatDate(expirationDate));
+//        String dateNeedFormat = "10Nov22";
+//        String expirationDate = null;
+//        String actualDate = formatDate(dateNeedFormat);
+//        String expectedDate = "2022-11-10";
+//        System.out.println(formatDate("2022-11-10"));
+//        System.out.println(actualDate);
+//        System.out.println(expectedDate);
+//        System.out.println(formatDate(expirationDate));
 //        String test = "I love java";
 //
 //        System.out.println(test.substring(0,1));
 //        System.out.println(test.substring(1));
-
+        int[] nums = {1,2,2,2,3,3,4,5,7,7,9,10,11,13,13,16,19,20};
+        //System.out.println(Arrays.toString(removeDuplicates(nums)));
+        System.out.println(Arrays.toString(findDupllicates(nums)) );
 
     }
 
@@ -54,22 +52,76 @@ public class removeDuplicates {
 
     }
 
-    private static int[] findDuplicate(int[] nums) {
+    private static int[] removeDuplicates(int[] nums) {
 
         int i = 0;
+        int sub = 0;
         for (int j = 1; j < nums.length; j++)
         {
             if (!(nums[i] == nums[j]))
             {
                 i++;
                 nums[i] = nums[j];
+                sub = i;
             }
 
         }
-        return nums;
+
+        int[] newNums = new int[sub+1];
+
+        for (int a = 0; a <= sub; a++)
+        {
+            newNums[a] = nums[a];
+        }
+
+
+        return newNums;
 
 
     }
+
+
+    // Find the duplicates in the array and return a new int[] of those duplicates
+    public static int[] findDupllicates(int[] nums)
+    {
+
+        //{1,2,2,2,3,3,4,5,7,7,9,10,11,13,13,16,19,20};
+
+        Map<Integer, Integer> numbers = new HashMap<>();
+
+
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            //Store value if not present in map,
+            if (!numbers.containsKey(nums[i]))
+            {
+                numbers.put(nums[i], 1);
+            }
+            // if already present, increase values by 1
+            else
+            {
+                numbers.computeIfPresent(nums[i], (k, v) -> v+1);
+            }
+        }
+
+        List<Integer> duplicates = new ArrayList<>();
+
+        // iterate through map to find any key that has value greater than 1
+        for (Map.Entry<Integer, Integer> entry : numbers.entrySet())
+        {
+            if (entry.getValue() > 1)
+            {
+                duplicates.add(entry.getKey());
+            }
+        }
+
+        //Convert ArrayList to int[]
+        int[] results = duplicates.stream().mapToInt(i -> i).toArray();
+
+        return results;
+    }
+
 
     public static String formatDate(String expirationDate) {
 
