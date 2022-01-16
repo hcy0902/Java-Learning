3cd97d340ee88c9c6a412b9bc5e47970b8a49526
Owen <owen.lattanzi@yahoo.com>
2022-01-15 23:34:21 -0600

    Jan 15th | LC 70, 198, 120

--- src/BackTracking.java
+++ src/BackTracking.java
@@ -2,6 +2,8 @@ import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//LC 21, 206, 77, 46, 784
 public class BackTracking {
 
 
@@ -18,7 +20,54 @@ public class BackTracking {
     }
 
 
+    //21 Merge two Sorted Lists
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode merge = new ListNode();
+        ListNode head = merge;
 
+        while(list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                head.next = list1;
+                list1 = list1.next;
+                head = head.next;
+            }
+            else
+            {
+                head.next = list2;
+                list2 = list2.next;
+                head = head.next;
+            }
+        }
+
+        head.next = list1==null?list2: list1;
+
+        return merge.next;
+
+
+    }
+
+    //LC 206 Reverse LinkedList
+    public ListNode reversList(ListNode head){
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while(current!= null)
+        {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+
+    }
+
+    //LC 77
     public List<List<Integer>> combine(int n, int k) {
 
         //create a return type of list
@@ -60,5 +109,88 @@ public class BackTracking {
 
     }
 
+
+    //LC 46 Permutations
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> results = new ArrayList<>();
+
+        dfs(results, nums, 0);
+
+        return results;
+
+    }
+
+    private void dfs(List<List<Integer>> results, int[] nums, int index) {
+
+        if (index >= nums.length)
+        {
+            ArrayList<Integer> result = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++)
+            {
+                result.add(nums[i]);
+            }
+            results.add(new ArrayList(result));
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++)
+        {
+            swap(nums, i, index);
+            dfs(results, nums, index +1);
+            swap(nums, index, i);
+        }
+
+    }
+
+    public void swap (int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] =  nums[j];
+        nums[j] =  temp;
+    }
+
+
+    //LC 784 Letter Case Permutation
+
+    public List<String> letterCasePermutation(String s) {
+
+        List<String> result = new ArrayList<>();
+
+        char[] chars = s.toCharArray();
+
+        dfw(result, chars, 0);
+        return result;
+    }
+
+    private void dfw(List<String> result, char[] chars, int index) {
+
+        if (index >= chars.length)
+        {
+            result.add(new String(chars));
+            return;
+        }
+
+        if (!Character.isLetter(chars[index]))
+        {
+            dfw(result, chars, index+1);
+            return;
+        }
+
+        chars[index] =  Character.toLowerCase(chars[index]);
+        dfw(result, chars, index+1);
+
+        chars[index] = Character.toUpperCase(chars[index]);
+        dfw(result, chars, index +1);
+
+    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 
 }
--- src/DP.java
+++ src/DP.java
@@ -114,9 +114,79 @@ public class DP {
 
     //LC 120 Triangle
     // dfs
+    Integer[][] seen;
     public int minimumTotalRewind(List<List<Integer>> triangle)
     {
-        return 0;
+        //DFS 的主要一个点在于由上到下去找，从上一层到下一层，只有两种情况下去，那么我们就DFS 顺着这两个下去
+
+        int result = 0;
+        //define seen
+        seen= new Integer[triangle.size()][triangle.size()];
+        // start from the top, [0,0]
+        result = dfs(triangle, 0, 0);
+
+
+
+
+        return result;
+
+    }
+
+    private int dfs(List<List<Integer>> triangle, int i, int j) {
+
+        if (i < 0 || j < 0 || i>= triangle.size() || j>= triangle.size())
+        {
+            return 0;
+        }
+
+        // if this has not been checked, we do the dfs call
+        if (seen[i][j] == null)
+        {
+            // return the min of two path + the current node's value at i, j
+            seen[i][j] = Math.min(dfs(triangle, i+1, j), dfs(triangle, i+1, j+1)) + triangle.get(i).get(j);
+        }
+
+        // if already checked, we skip dfs, just return its value
+        return seen[i][j];
+
+        //Above is good, but we need to keep in mind of time limitation, we can optimize this by skipping the repetitive
+        //check, for example, 5 is checked twice by 3 and 4, we can mark 5 as seen so it only got checked once
+        //   2
+        //  3 4
+        // 6 5 7
+        //4 1 8 3
+
+    }
+
+    //LC 12
+    //DP
+    public int minimumTotalDp(List<List<Integer>> triangle)
+    {
+        // the main idea of using DP is to start from bottom to up, we need a 1d dp array to first store the
+        //last row of the triangle, and work our way up, from bottom there is only two possible way going up
+        // and that is what we are using
+        int[] dp = new int[triangle.size()];
+
+        //loop through the last row and put all into dp as initial values
+        for (int i = 0; i < triangle.size(); i++)
+        {
+            dp[i] = triangle.get(triangle.size()-1).get(i);
+        }
+
+        // work out way up from the second to the last row
+        for (int i = triangle.size()-2; i>= 0; i--)
+        {
+            // This loop is for the each element on each row
+            for(int j = 0; j<=i; j++)
+            {
+                // this target can only have two path from bottom, j and j+1, we take the lesser value and add the current
+                //target value
+                dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+            }
+        }
+
+        return dp[0];
+
     }
 
 }
